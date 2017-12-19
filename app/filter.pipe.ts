740cb703dbde6292628e93fdd3ce2624b8cdf449
Guest User <Guest@Epicodus-4D.local>
2017-12-18 16:19:58 -0800

    add tweaks to sort by age(broken)

--- app/filter.pipe.ts
+++ app/filter.pipe.ts
@@ -5,20 +5,48 @@ import { Animal } from './animal.model';
   pure: false
 })
 export class FilterPipe implements PipeTransform {
-  transform(oldRoster:Animal[],type:Number) {
+  transform(oldRoster:Animal[],type:number) {
     let newRoster:Animal[] = [];
     oldRoster.forEach(animal => {
+      // switch (type)
+      // {
+      //   case 1:
+      //     if(animal.gender==="M"){ newRoster.push(animal); }
+      //     break;
+      //   case 2:
+      //     if(animal.gender==="F"){ newRoster.push(animal); }
+      //     break;
+      // }
       switch (type)
       {
+        //youngest first
         case 1:
-          if(animal.gender==="M"){ newRoster.push(animal); }
-          break;
+          let lowest = 0;
+          while (oldRoster.length>0) {
+            oldRoster.forEach((x,i) => {
+              if (oldRoster[lowest].age>x.age)
+              { lowest = i; }
+            });
+            newRoster.push(oldRoster.splice(lowest,1)[0]);
+          }
+          return newRoster;
+          //oldest first
         case 2:
-          if(animal.gender==="F"){ newRoster.push(animal); }
-          break;
+          let highest = 0;
+          while (oldRoster.length>0) {
+            oldRoster.forEach((x,i) => {
+              if (oldRoster[highest].age<x.age)
+              { highest = i; }
+            });
+            newRoster.push(oldRoster.splice(highest,1)[0]);
+          }
+          return newRoster;
+          //unsorted
+        default:
+          return oldRoster;
       }
     });
-    if(newRoster.length!=0){return newRoster;}
-    else{return oldRoster;}
+    // if(newRoster.length!=0){return newRoster;}
+    // else{return oldRoster;}
   }
 }
